af25eaceb29393c691a5c3c342be1f96edccc8cf
dvan1@utpl.edu.ec <dvan1@utpl.edu.ec>
2023-01-18 10:22:27 -0500

    Commit clase

--- Ejemplos/src/paquete03/Principal.java
+++ Ejemplos/src/paquete03/Principal.java
@@ -7,6 +7,8 @@ package paquete03;
 
 import paquete04.OperadorUno;
 import paquete04.OperadorDos;
+import paquete05.OperadorTres;
+//import paquete04.*;
 
 /**
  *
@@ -18,9 +20,14 @@ public class Principal {
         int valorA = OperadorUno.obtenerNumero(100);
         int valorB = OperadorUno.obtenerNumero(10);
         int suma = OperadorDos.obtenerSuma(valorA, valorB);
-        System.out.printf("La suma de %d + %d es igual a: %d\n",
+        int multiplicacion = OperadorTres.Obtenermultiplicacion(valorA, valorB);
+        System.out.printf("La suma de %d + %d es igual a: %d\nLa multiplicacion "
+                + "de %d * %d es igual a: %d\n",
                 valorA,
                 valorB,
-                suma);
+                suma,
+                valorA,
+                valorB,
+                multiplicacion);
     }
 }
